3f68dce3a2bbd34055700487eb5731a3ffadd261
Evgenei <junaers1996@gmail.com>
2022-05-31 21:42:15 +0300

    feat: не рабочий вывод csv файла

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+import csv
 
 
 def index(request):
@@ -7,11 +8,17 @@ def index(request):
 
 
 def bus_stations(request):
+    csvfile = open('data-398-2018-08-30.csv', newline='', encoding='utf-8')
+    reader = csv.DictReader(csvfile)
+    print(reader())
+    for row in reader:
+        print(row['Street'], row['District'])
+
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': row,
+
     }
     return render(request, 'stations/index.html', context)
