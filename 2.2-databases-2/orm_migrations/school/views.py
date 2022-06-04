25c421908c40378341b88399ad66fd8cce8a8996
junaer <junaers1996@gmail.com>
2022-06-04 19:46:15 +0300

    refactor: model add m2m connection and small fix html

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,12 +1,18 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student
+from .models import Student, Teacher
 
 
 def students_list(request):
+    object_list = Student.objects.all()
     template = 'school/students_list.html'
-    context = {}
+    context = {'object_list': object_list}
+    for student in object_list:
+        print(f'firts {student.name}')
+        for teacher in student.teachers.all():
+            print(f'second {teacher.name}, {teacher.subject}')
+
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -73,9 +73,11 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_orm_migrations',
+        'NAME': 'django-db',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'junaer',
+        'PASSWORD': '',
     }
 }
 
@@ -102,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'RU-ru'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
